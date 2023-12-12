387928a5c343aeca0193a471c3ad3179ac5373dc
tohnperfect <thanapong.intharah@gmail.com>
2023-12-12 11:01:45 +0700

    add argparse

--- firstpy.py
+++ firstpy.py
@@ -1,3 +1,14 @@
+import argparse
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--num',
+        type=int,
+        required=True,
+        help='input for the multiplyby9 function'
+    )
 
 def printHello():
     print("Hello World!")
@@ -6,7 +17,8 @@ def multiplyby9(inputV):
     print(9*inputV)
 
 if __name__ == "__main__":
+    input_v = parse_input()
+    print(f'the input num is {input_v.num}')
     print('we are in the main function')
     multiplyby9(20)
     printHello()
-    
