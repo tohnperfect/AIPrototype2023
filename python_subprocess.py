2be4dcefbdb7f13d1898b499910b44a10d8d843f
tohnperfect <thanapong.intharah@gmail.com>
2023-12-25 15:08:22 +0700

    add delete folder

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,5 +1,8 @@
+from re import sub
 import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     # basic terminal command
     subprocess.run(["ls","-ltr"])
+    subprocess.run(["rm","-r","~/testfolder1"])
+    subprocess.run(["cd"])
