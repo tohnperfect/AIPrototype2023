06763dd8b3f6b7fab9d3a15ce5bd6b65ee741c0c
tohnperfect <thanapong.intharah@gmail.com>
2023-12-25 15:33:32 +0700

    run .py

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,5 +3,13 @@ import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
     # basic terminal command
-    subprocess.run(["ls","-ltr"])
-    subprocess.run(["rm","-r","/home/tohn/testfolder1"])
+    #subprocess.run(["ls","-ltr"])
+    #subprocess.run(["rm","-r","/home/tohn/testfolder1"])
+
+    #python commands
+    print(f"first run num=100 XX=90")
+    subprocess.run(["python","firstpy.py", "--num", "100", "--XX", "90"])
+    print(f"second run num=-10 XX=-90")
+    subprocess.run(["python","firstpy.py", "--num", "-10", "--XX", "-90"])
+    print(f"first run num=0")
+    subprocess.run(["python","firstpy.py", "--num", "0"])
