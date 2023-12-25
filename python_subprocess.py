d562047bed9bdac25ad2cc747c878b9995d4e8ef
tohnperfect <thanapong.intharah@gmail.com>
2023-12-25 16:06:15 +0700

    run with output

--- python_subprocess.py
+++ python_subprocess.py
@@ -16,3 +16,13 @@ if __name__ == "__main__":
     print(f"third run num=0")
     subprocess.run(["python","firstpy.py", "--num", "0"])
     print(f"------------------------------------------------")
+
+
+
+    #use output from other program
+    process_output = subprocess.Popen(["python","firstpy.py", "--num", "0"],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+    out, err = process_output.communicate()
+    print(out.decode('utf-8'))
+    print(len(out.decode('utf-8')))
