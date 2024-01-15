d8e1c681050f4d4aaf7bbdd5aa6b338449c0664c
tohnperfect <thanapong.intharah@gmail.com>
2024-01-15 16:05:26 +0700

    link with html

--- firstflask.py
+++ firstflask.py
@@ -12,5 +12,9 @@ def helloworld():
 def hellotohn():
     return "Hello, Tohn!"
 
+@app.route("/home2")
+def home2():
+    return render_template("home.html",name='tohn')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
