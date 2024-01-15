757f8c9d3fb9cf7fb96ef294f3860e4c77f3c905
tohnperfect <thanapong.intharah@gmail.com>
2024-01-15 15:42:30 +0700

    add more function

--- firstflask.py
+++ firstflask.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def helloworld():
     return "Hello, World!"
 
+@app.route("/name")  
+def hellotohn():
+    return "Hello, Tohn!"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
