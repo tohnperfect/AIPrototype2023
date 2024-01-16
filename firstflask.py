0119d4f71e52147ef3514089e79866cf07bafb19
tohnperfect <thanapong.intharah@gmail.com>
2024-01-16 11:12:13 +0700

    test upload file

--- firstflask.py
+++ firstflask.py
@@ -27,6 +27,24 @@ def homefn():
         print(namein, file=sys.stdout)
         print(lastnamein, file=sys.stdout)
         return render_template("home.html",name=namein)
+    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']       
+        file.save('filename')
+        return render_template("home.html",name='upload completed')
+        
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
